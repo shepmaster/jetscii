25e62c77722b8ec37432062ccaf3103e37c591d4
Zachary Dremann <dremann@gmail.com>
2023-10-09 00:48:32 -0400

    Add a benchmark for a full 16 chars

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -1,11 +1,12 @@
 use criterion::{criterion_group, criterion_main, Criterion, Throughput};
 use jetscii::{ascii_chars, AsciiCharsConst, SubstringConst};
 use std::hint::black_box;
-use std::sync::OnceLock;
 
 static SPACE: AsciiCharsConst = ascii_chars!(' ');
 static XML_DELIM_3: AsciiCharsConst = ascii_chars!('<', '>', '&');
 static XML_DELIM_5: AsciiCharsConst = ascii_chars!('<', '>', '&', '\'', '"');
+static BIG_16: AsciiCharsConst =
+    ascii_chars!('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P');
 
 static SUBSTRING: SubstringConst = SubstringConst::new("xyzzy");
 
@@ -109,14 +110,84 @@ fn xml5(c: &mut Criterion) {
     });
 }
 
-fn memchr_finder() -> &'static memchr::memmem::Finder<'static> {
-    static FINDER: OnceLock<memchr::memmem::Finder> = OnceLock::new();
-    FINDER.get_or_init(|| memchr::memmem::Finder::new("xyzzy"))
-}
+fn big_16(c: &mut Criterion) {
+    let mut haystack = prefix_string();
+    haystack.push('P');
+    let haystack = black_box(haystack);
 
-fn substring() -> &'static SubstringConst {
-    static SUBSTRING: OnceLock<SubstringConst> = OnceLock::new();
-    SUBSTRING.get_or_init(|| SubstringConst::new("xyzzy"))
+    let mut group = c.benchmark_group("find_big_16");
+    group.throughput(Throughput::Bytes(haystack.len() as u64));
+
+    group.bench_function("ascii_chars", |b| {
+        b.iter(|| BIG_16.find(&haystack));
+    });
+    group.bench_function("stdlib_find_char_set", |b| {
+        b.iter(|| {
+            haystack.find(
+                &[
+                    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+                ][..],
+            )
+        });
+    });
+    group.bench_function("stdlib_find_closure", |b| {
+        b.iter(|| {
+            haystack.find(|c| {
+                c == 'A'
+                    || c == 'B'
+                    || c == 'C'
+                    || c == 'D'
+                    || c == 'E'
+                    || c == 'F'
+                    || c == 'G'
+                    || c == 'H'
+                    || c == 'I'
+                    || c == 'J'
+                    || c == 'K'
+                    || c == 'L'
+                    || c == 'M'
+                    || c == 'N'
+                    || c == 'O'
+                    || c == 'P'
+            })
+        });
+    });
+    group.bench_function("stdlib_iter_position", |b| {
+        b.iter(|| {
+            haystack.bytes().position(|c| {
+                c == b'A'
+                    || c == b'B'
+                    || c == b'C'
+                    || c == b'D'
+                    || c == b'E'
+                    || c == b'F'
+                    || c == b'G'
+                    || c == b'H'
+                    || c == b'I'
+                    || c == b'J'
+                    || c == b'K'
+                    || c == b'L'
+                    || c == b'M'
+                    || c == b'N'
+                    || c == b'O'
+                    || c == b'P'
+            })
+        });
+    });
+    group.bench_function("memchr", |b| {
+        b.iter(|| {
+            let bytes = haystack.as_bytes();
+            let indexes = [
+                memchr::memchr3(b'A', b'B', b'C', bytes),
+                memchr::memchr3(b'D', b'E', b'F', bytes),
+                memchr::memchr3(b'G', b'H', b'I', bytes),
+                memchr::memchr3(b'J', b'K', b'L', bytes),
+                memchr::memchr3(b'M', b'N', b'O', bytes),
+                memchr::memchr(b'P', bytes),
+            ];
+            indexes.iter().copied().flatten().min()
+        })
+    });
 }
 
 fn substr(c: &mut Criterion) {
@@ -139,5 +210,5 @@ fn substr(c: &mut Criterion) {
     });
 }
 
-criterion_group!(benches, spaces, xml3, xml5, substr);
+criterion_group!(benches, spaces, xml3, xml5, big_16, substr);
 criterion_main!(benches);
