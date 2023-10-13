0c426090254390f6731a50e4f182c30b15075f15
Zachary Dremann <dremann@gmail.com>
2023-10-13 17:55:32 -0400

    Add benchmark for worst case for memchr
    Add a test that looks for the first item in a long haystack

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -188,6 +188,85 @@ fn big_16(c: &mut Criterion) {
             indexes.iter().copied().flatten().min()
         })
     });
+
+    group.finish();
+
+    let mut haystack = prefix_string();
+    haystack.insert(0, 'P');
+    let haystack = black_box(haystack);
+    let mut group = c.benchmark_group("find_big_16_early_return");
+    group.throughput(Throughput::Bytes(1));
+
+    group.bench_function("ascii_chars", |b| {
+        b.iter(|| BIG_16.find(&haystack));
+    });
+    group.bench_function("stdlib_find_char_set", |b| {
+        b.iter(|| {
+            haystack.find(
+                &[
+                    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+                ][..],
+            )
+        });
+    });
+    group.bench_function("stdlib_find_closure", |b| {
+        b.iter(|| {
+            haystack.find(|c| {
+                c == 'A'
+                    || c == 'B'
+                    || c == 'C'
+                    || c == 'D'
+                    || c == 'E'
+                    || c == 'F'
+                    || c == 'G'
+                    || c == 'H'
+                    || c == 'I'
+                    || c == 'J'
+                    || c == 'K'
+                    || c == 'L'
+                    || c == 'M'
+                    || c == 'N'
+                    || c == 'O'
+                    || c == 'P'
+            })
+        });
+    });
+    group.bench_function("stdlib_iter_position", |b| {
+        b.iter(|| {
+            haystack.bytes().position(|c| {
+                c == b'A'
+                    || c == b'B'
+                    || c == b'C'
+                    || c == b'D'
+                    || c == b'E'
+                    || c == b'F'
+                    || c == b'G'
+                    || c == b'H'
+                    || c == b'I'
+                    || c == b'J'
+                    || c == b'K'
+                    || c == b'L'
+                    || c == b'M'
+                    || c == b'N'
+                    || c == b'O'
+                    || c == b'P'
+            })
+        });
+    });
+    group.bench_function("memchr", |b| {
+        b.iter(|| {
+            let bytes = haystack.as_bytes();
+            let indexes = [
+                memchr::memchr3(b'A', b'B', b'C', bytes),
+                memchr::memchr3(b'D', b'E', b'F', bytes),
+                memchr::memchr3(b'G', b'H', b'I', bytes),
+                memchr::memchr3(b'J', b'K', b'L', bytes),
+                memchr::memchr3(b'M', b'N', b'O', bytes),
+                memchr::memchr(b'P', bytes),
+            ];
+            indexes.iter().copied().flatten().min()
+        })
+    });
 }
 
 fn substr(c: &mut Criterion) {
